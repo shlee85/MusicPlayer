6c9b4e4f3adb3083525bbd97f2bd701f84a8f43c
shlee85 <shlee85@lowasis.com>
2023-03-16 18:24:13 +0900

    필요한 아이콘 추가 및 레이아웃 추가

--- app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.simplemusicplayer
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    val mServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.d(TAG, "서비스 연결 성공")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.d(TAG, "서비스 연결 실패")
+        }
+    }
+
+    private fun bindService() {
+        val intent = Intent(this, AudioPlayerService::class.java)
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
